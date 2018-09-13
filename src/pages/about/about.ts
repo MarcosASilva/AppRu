efecc7e80c2cfe356b13361a947d4c7ac021a29c
marcos <marcosvlk15@hmail.co>
2018-09-13 15:13:17 -0300

    ..

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,67 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  public event = {
+    month: '2018-09-10',
+
+  }
+
+  barChartOptions: any = [{
+    scales: {
+       yAxes: [
+        {
+            display: true,
+            ticks: {
+              fontSize: 10,
+              colors: '#fff'
+            }
+        }
+      ]
+    }
+  }];
+  barChartLabels = [];
+  barChartType:string = 'bar';
+  barChartLegend:boolean = true;
+  barChartData:any;
+ 
+
+  doughnutChartLabels:string[];
+  doughnutChartData:number[];
+  doughnutChartType:string = 'pie';
+  
   constructor(public navCtrl: NavController) {
 
+  }
+
+  calc(tipo){
+    if (tipo === 'line') {
+        this.calcBar();
+    } else {
+        this.calcDoughnut();
+    }
+  }
+  
+  calcBar(){
+    this.barChartLabels = ['11:00', '11:15', '11:30', '11:45','12:00', '12:15', '12:20', '12:30'];      
+    this.barChartData = [
+  	  {data: [15,25,50, 10,10, 10,7,13], label: 'Ótimo'},
+        {data: [20,20,15,25,40,30,15,35], label: 'Bom'},
+        {data: [50,100,300,50,25, 235, 40,20 ], label: 'Ruim'},
+        {data: [450, 750, 250,150, 300,100,350,350], label: 'Péssimo'}
+        
+    ];
+  }
+  
+  calcDoughnut(){
+    this.doughnutChartLabels = ['Ótimo', 'Bom', 'Ruim', 'Péssimo'];
+    this.doughnutChartData = [140, 200,820,2700];
+  };
+  monthChange(val: any) {
+    console.log('Month Change:', val);
+  }
+
+  dayChange(val: any) {
+    console.log('Day Change:', val);
   }
 
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,8 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  lista = []
   constructor(public navCtrl: NavController) {
+    this.lista=[
+      {cardapio:"Feijão Tropeiro",valor:"1200 pts"},
+      {cardapio:"Feijoada",valor:"1000 pts"},
+      {cardapio:"Strogonoff",valor:"850 pts"},
+      {cardapio:"Steak",valor:"700 pts"},
+      {cardapio:"Carne Moída",valor:"575 pts"},
+      {cardapio:"Almondega",valor:"550 pts"},
+      {cardapio:"Carne da vovó ",valor:"300 pts"},
+
+
+    ];
 
   }
 
