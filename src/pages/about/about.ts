0c06ccea2ed69b9a6ddaf92435e7500313c2cf33
marcos <marcosvlk15@hmail.co>
2018-09-14 01:21:26 -0300

    ..

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -11,63 +11,119 @@ export class AboutPage {
     month: '2018-09-10',
 
   }
+  day = "13";
+  month = "09"; 
+  year = "2018";
+  almoco = "Costelinha";
+  ref = "a";
+  t = "line";
+  listac = ["a","b","c","d","e","f","g" ,"h"]
 
   barChartOptions: any = [{
     scales: {
-       yAxes: [
+      yAxes: [
         {
-            display: true,
-            ticks: {
-              fontSize: 10,
-              colors: '#fff'
-            }
+          display: true,
+          ticks: {
+            fontSize: 10,
+            colors: '#fff'
+          }
         }
       ]
     }
   }];
   barChartLabels = [];
-  barChartType:string = 'bar';
-  barChartLegend:boolean = true;
-  barChartData:any;
- 
+  barChartType: string = 'bar';
+  barChartLegend: boolean = true;
+  barChartData: any;
 
-  doughnutChartLabels:string[];
-  doughnutChartData:number[];
-  doughnutChartType:string = 'pie';
-  
-  constructor(public navCtrl: NavController) {
 
+  doughnutChartLabels: string[];
+  doughnutChartData: number[];
+  doughnutChartType: string = 'pie';
+
+  constructor(public navCtrl: NavController, ) {
+    this.calcBar();
   }
 
-  calc(tipo){
+  calc(tipo) {
     if (tipo === 'line') {
-        this.calcBar();
+      this.calcBar();
+      this.t='line';
     } else {
-        this.calcDoughnut();
+      this.t = 'a';
+      this.calcDoughnut();
     }
   }
+
+  calcBar() {
+    this.barChartLabels = [];
+    if(this.ref==="a"){
+      this.barChartLabels = ['11:00', '11:30', '12:00', '12:30'];
+      this.barChartData = [
+        { data: [15, 25, 50, 10], label: 'Ótimo' },
+        { data: [20, 20, 15, 25], label: 'Bom' },
+        { data: [50, 100, 300, 50], label: 'Ruim' },
+        { data: [450, 750, 250, 150], label: 'Péssimo' }
   
-  calcBar(){
-    this.barChartLabels = ['11:00', '11:15', '11:30', '11:45','12:00', '12:15', '12:20', '12:30'];      
-    this.barChartData = [
-  	  {data: [15,25,50, 10,10, 10,7,13], label: 'Ótimo'},
-        {data: [20,20,15,25,40,30,15,35], label: 'Bom'},
-        {data: [50,100,300,50,25, 235, 40,20 ], label: 'Ruim'},
-        {data: [450, 750, 250,150, 300,100,350,350], label: 'Péssimo'}
-        
-    ];
+      ];
+    }
+    else{
+      this.barChartLabels = ['17:45','18:15','18:30','18:45'];
+      this.barChartData = [
+        { data: [50,20,10,5], label: 'Ótimo' },
+        { data: [120, 40, 40,10], label: 'Bom' },
+        { data: [30, 60, 30,15], label: 'Ruim' },
+        { data: [50, 75, 45,20], label: 'Péssimo' }
+  
+      ];
+    }
   }
-  
-  calcDoughnut(){
-    this.doughnutChartLabels = ['Ótimo', 'Bom', 'Ruim', 'Péssimo'];
-    this.doughnutChartData = [140, 200,820,2700];
+
+  calcDoughnut() {
+    if(this.ref==="a"){
+      this.doughnutChartLabels = ['Ótimo', 'Bom', 'Ruim', 'Péssimo'];
+      this.doughnutChartData = [140, 200, 820, 2700];
+    }
+    else{
+      this.doughnutChartLabels = ['Ótimo', 'Bom', 'Ruim', 'Péssimo'];
+      this.doughnutChartData = [80, 200, 120, 170];
+    }
   };
+  
   monthChange(val: any) {
     console.log('Month Change:', val);
+    this.month = val;
   }
 
   dayChange(val: any) {
     console.log('Day Change:', val);
+    this.day = val;
+  }
+  yearChange(val: any) {
+    console.log('Day Change:', val);
+    this.year = val;
+  }
+  refeicao(val: any) {
+    console.log('Day Change:', val);
+    this.ref = val;
+    if(this.ref==="a"){
+      this.almoco = "Costelinha";
+      if(this.t==="line"){
+        this.calc('line');
+      }else{
+        this.calc('a');
+      }
+    }
+    else{
+      this.almoco = "Bife de Palmas";
+      if(this.t==="line"){
+        this.calc('line');
+      }else{
+        this.calc('a');
+      }
+    }
+
   }
 
 }
--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -16,10 +16,81 @@ export class ContactPage {
       {cardapio:"Carne Moída",valor:"575 pts"},
       {cardapio:"Almondega",valor:"550 pts"},
       {cardapio:"Carne da vovó ",valor:"300 pts"},
+      {cardapio:"Feijão Tropeiro",valor:"1200 pts"},
+      {cardapio:"Feijoada",valor:"1000 pts"},
+      {cardapio:"Strogonoff",valor:"850 pts"},
+      {cardapio:"Steak",valor:"700 pts"},
+      {cardapio:"Carne Moída",valor:"575 pts"},
+      {cardapio:"Almondega",valor:"550 pts"},
+      {cardapio:"Carne da vovó ",valor:"300 pts"},
+      {cardapio:"Feijão Tropeiro",valor:"1200 pts"},
+      {cardapio:"Feijoada",valor:"1000 pts"},
+      {cardapio:"Strogonoff",valor:"850 pts"},
+      {cardapio:"Steak",valor:"700 pts"},
+      {cardapio:"Carne Moída",valor:"575 pts"},
+      {cardapio:"Almondega",valor:"550 pts"},
+      {cardapio:"Carne da vovó ",valor:"300 pts"},
+      {cardapio:"Feijão Tropeiro",valor:"1200 pts"},
+      {cardapio:"Feijoada",valor:"1000 pts"},
+      {cardapio:"Strogonoff",valor:"850 pts"},
+      {cardapio:"Steak",valor:"700 pts"},
+      {cardapio:"Carne Moída",valor:"575 pts"},
+      {cardapio:"Almondega",valor:"550 pts"},
+      {cardapio:"Carne da vovó ",valor:"300 pts"},
 
 
     ];
 
   }
+  calc(tipo){
+    if (tipo === 'a') {
+        this.calcA();
+    } else {
+        this.calcJ();
+    }
+  }
+  calcA(){
+    this.lista=[
+      {cardapio:"Feijão Tropeiro",valor:"1200 pts"},
+      {cardapio:"Feijoada",valor:"1000 pts"},
+      {cardapio:"Strogonoff",valor:"850 pts"},
+      {cardapio:"Steak",valor:"700 pts"},
+      {cardapio:"Carne Moída",valor:"575 pts"},
+      {cardapio:"Almondega",valor:"550 pts"},
+      {cardapio:"Carne da vovó ",valor:"300 pts"},
+      {cardapio:"Feijão Tropeiro",valor:"1200 pts"},
+      {cardapio:"Feijoada",valor:"1000 pts"},
+      {cardapio:"Strogonoff",valor:"850 pts"},
+      {cardapio:"Steak",valor:"700 pts"},
+      {cardapio:"Carne Moída",valor:"575 pts"},
+      {cardapio:"Almondega",valor:"550 pts"},
+      {cardapio:"Carne da vovó ",valor:"300 pts"},
+      {cardapio:"Feijão Tropeiro",valor:"1200 pts"},
+      {cardapio:"Feijoada",valor:"1000 pts"},
+  
+
+
+    ];
+  }
+  calcJ(){
+    this.lista=[
+      {cardapio:"Strogonoff",valor:"850 pts"},
+  {cardapio:"Steak",valor:"700 pts"},
+  {cardapio:"Carne Moída",valor:"575 pts"},
+  {cardapio:"Almondega",valor:"550 pts"},
+  {cardapio:"Carne da vovó ",valor:"300 pts"},
+  {cardapio:"Feijão Tropeiro",valor:"1200 pts"},
+  {cardapio:"Feijoada",valor:"1000 pts"},
+  {cardapio:"Strogonoff",valor:"850 pts"},
+  {cardapio:"Steak",valor:"700 pts"},
+  {cardapio:"Carne Moída",valor:"575 pts"},
+  {cardapio:"Almondega",valor:"550 pts"},
+  {cardapio:"Carne da vovó ",valor:"300 pts"},
+  
+
+
+    ];
+  }
+
 
 }
